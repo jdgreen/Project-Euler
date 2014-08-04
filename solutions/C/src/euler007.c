2e358ae9274a1a96232e6b19dd66b4b8880fe60e
green <jonathangreen07@gmail.com>
2014-08-04 20:02:26 +0100

    New euler007.c

--- solutions/C/src/euler007.c
+++ solutions/C/src/euler007.c
@@ -1,31 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 // What is the 10001st prime number?
 
 #define NUM_PRI 10001
 
-//check if prime number
-int prime(int num)
-{
-	for(int i=(int) sqrt(num); i>1; i--){
-		if(num % i == 0) return 0;
-	}
-	return 1;
-}
-
 int main(int argc, char const *argv[])
 {
 	//initialise variables
-	int count=0, num=1;
+	int num = 3, count=1, pri;
 
-	//while loop to determine prime number
+	//remembering to cast the malloc with a C++ compiler
+	int *primes = (int *) malloc( sizeof(int) );
+	primes[0] = 2; //assigning the first value to 2
 
-	while( count <= NUM_PRI ){
-		if( prime(num) == 1 ) count += 1;
-		num += 1;
+	//logic accounts for when there is insufficient memory
+	if( primes != NULL ){
+		while(count < NUM_PRI){
+
+			//resetting values
+			pri=1;
+
+			//for loop determining divisible factors
+			for ( int a = 0; a < count; a++){
+				if( num % primes[a] == 0) {
+					pri = 0;
+					break;
+				}
+			}
+
+			//updating array with a prime number
+			if( pri == 1 ){
+				count += 1;
+				primes = (int*) realloc( primes, count*sizeof(int) );
+				primes[count-1] = num;
+			}
+		num++;
+		}
+
+		// clear memory and finish off
+		free(primes);
+		printf("%d\n", primes[NUM_PRI-1]);
+		return 0;
 	}
-	printf("%d\n", num-1);
-
-	return 0;
+	else{
+		printf("!!! Insufficient memory\n");
+		return 1;		
+	}
 }
